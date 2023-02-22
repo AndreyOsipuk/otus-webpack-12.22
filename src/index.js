fa6ea57dd4b78e306548ccf4f7a0801de098d453
Andrey Osipuk <andrey.o@list.ru>
2023-02-22 22:46:41 +0400

    add webpack config

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+import cat from './images/cat.jpeg'
+const el = document.body
+
+// el.innerHTML = 'Hello, Webpack!!!!'
+document.querySelector('img').src = cat
+
+console.log(1234)
+
+const foo = () => 1
